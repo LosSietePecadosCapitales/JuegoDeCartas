019b995d9caa354f1821c9530e6449e34d387112
Daniel <daniel.ubilla.c@gmail.com>
2021-05-18 23:48:17 -0400

    Comentado Game, ConnectionMySQL

--- src/Features/Objects/Game.java
+++ src/Features/Objects/Game.java
@@ -9,7 +9,7 @@ import javafx.beans.property.SimpleStringProperty;
 
 /**
  *
- * @author Vicente
+ * @author LosSietePecadosCapitales
  */
 public class Game {
     private String ip;
@@ -17,7 +17,14 @@ public class Game {
     private String pass;
     private String hasPass;
     private String owner;
-
+/**
+ * Constructor del juego, utilizado para buscar y unirse a partidas
+ * @param ip direccion de ip del jugador, identifica las partidas
+ * @param name nombre de la partida
+ * @param pass contraseña de la partida
+ * @param hasPass determina el cambio de turno
+ * @param owner quien creo la partida
+ */
     public Game(String ip, String name, String pass, String hasPass, String owner) {
         this.ip = ip;
         this.name = name;
@@ -25,43 +32,73 @@ public class Game {
         this.hasPass = hasPass;
         this.owner = owner;
     }
-
+    /**
+     * Obtiene la ip del objeto
+     * @return string del atributo ip
+     */
     public String getIp() {
         return ip;
     }
-
+    /**
+     * Asigna un valor a la variable ip
+     * @param ip 
+     */
     public void setIp(String ip) {
         this.ip = ip;
     }
-
+    /**
+     * Obtiene el nombre del objeto
+     * @return string del atributo name
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * Asigna un valor al atributo name
+     * @param name 
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    /**
+     * Obtiene el pass del objeto
+     * @return string del atributo pass
+     */
     public String getPass() {
         return pass;
     }
-
+    /**
+     * Asigna un valor al atributo pass
+     * @param pass 
+     */
     public void setPass(String pass) {
         this.pass = pass;
     }
-
+    /**
+     * Obtiene el valor de hasPass
+     * @return 
+     */
     public String getHasPass() {
         return hasPass;
     }
-
+    /**
+     * Asigna un valor al atributo hasPass
+     * @param hasPass 
+     */
     public void setHasPass(String hasPass) {
         this.hasPass = hasPass;
     }
-
+    /**
+     * Obtiene el valor de owner
+     * @return 
+     */
     public String getOwner() {
         return owner;
     }
-
+    /**
+     * Asigna un valor a owner
+     * @param owner 
+     */
     public void setOwner(String owner) {
         this.owner = owner;
     }
